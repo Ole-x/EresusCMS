c0d09a35ed7b74878fa644495d13b3b100a79789
Михаил Красильников <m.krasilnikov@yandex.ru>
2014-01-09 22:18:33 +0400

    Удалён неиспользуемый автозагрузчик.

--- src/core/Kernel.php
+++ src/core/Kernel.php
@@ -26,15 +26,13 @@
  * @package Eresus
  */
 
-
 /**
  * Ядро CMS
  *
  * Основные функции ядра
  * 1. запуск {@link Eresus_CMS основного класса приложения};
  * 2. перехват ошибок и исключений;
- * 3. {@link autoload() автозагрузка классов};
- * 4. получение основных сведений о системе.
+ * 3. получение основных сведений о системе.
  *
  * @package Eresus
  * @since 3.00
@@ -314,72 +312,6 @@ class Eresus_Kernel
             str_repeat('x', self::MEMORY_OVERFLOW_BUFFER_SIZE * 1024);
 
         // возвращаем false для вывода буфера
-        return false;
-    }
-
-    /**
-     * Автозагрузчик классов
-     *
-     * Работает только для классов «Eresus_*». Из имени класса удаляется префикс «Eresus_», все
-     * символы в имени класса «_» заменяются на разделитель директорий, добавляется суффикс «.php».
-     *
-     * Таким образом класс «Eresus_HTTP_Request» будет искаться в файле «core/HTTP/Request.php».
-     *
-     * Устанавливается через {@link spl_autoload_register() spl_autoload_register()} в методе
-     * {@link init()}.
-     *
-     * @param string $className
-     *
-     * @throws LogicException если класс не найден
-     *
-     * @return bool
-     *
-     * @since 3.00
-     * @uses classExists()
-     */
-    public static function autoload($className)
-    {
-        /*
-         * Классы Eresus
-         */
-        if (stripos($className, 'Eresus_') === 0)
-        {
-            $fileName = dirname(__FILE__) . DIRECTORY_SEPARATOR .
-                str_replace('_', DIRECTORY_SEPARATOR, substr($className, 7)) . '.php';
-
-            if (file_exists($fileName))
-            {
-                /** @noinspection PhpIncludeInspection */
-                include $fileName;
-                return self::classExists($className);
-            }
-            /*
-             * Doctrine при загрузке сущностей ищет необязательный класс с суффиксом «Table».
-             * Отсутствие такого класса не является ошибкой. Отсутствие любого другого класса расцениваем
-             * как логическую ошибку.
-             */
-            elseif (substr($className, -5) !== 'Table')
-            {
-                throw new LogicException('Class "' . $className . '" not found');
-            }
-        }
-
-        /*
-         * Классы Botobor
-         */
-        if (stripos($className, 'Botobor') === 0)
-        {
-            $fileName = dirname(__FILE__) . DIRECTORY_SEPARATOR . 'botobor' . DIRECTORY_SEPARATOR .
-                'botobor.php';
-
-            if (file_exists($fileName))
-            {
-                /** @noinspection PhpIncludeInspection */
-                include $fileName;
-                return self::classExists($className);
-            }
-        }
-
         return false;
     }
 
